ae886173bd82168944b5a22932ecbe12af18bb55
Mike Vermeulen <mike@vermeulen.com>
2023-12-30 10:57:03 -0600

    Add quotes around CCOPTS.

--- workload/phoronix/stream/install.sh
+++ workload/phoronix/stream/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CC=/opt/AMD/aocc-compiler-4.1.0/bin/clang CCOPTS=-fopenmp -O2 -mcmodel=large -ffp-contract=fast -fnt-store phoronix-test-suite force-install stream
+CC=/opt/AMD/aocc-compiler-4.1.0/bin/clang CCOPTS="-fopenmp -O2 -mcmodel=large -ffp-contract=fast -fnt-store" phoronix-test-suite force-install stream
