ba29f972b4d65ce54d3eae67b95d00416a5e6328
Mike Vermeulen <mike@vermeulen.com>
2023-12-30 13:50:50 -0600

    Update options for Intel.

--- workload/phoronix/stream/run.sh
+++ workload/phoronix/stream/run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-/home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --no-ipc --branch phoronix-test-suite batch-run stream
-/home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc  --topdown2 --no-rusage --no-software phoronix-test-suite batch-run stream
-/home/mev/source/wspy/topdown -o l2.float.txt --no-software --no-ipc --no-rusage --cache2 --float phoronix-test-suite batch-run stream
+if [ $(grep -c Intel /proc/cpuinfo) -gt 0 ]; then
+    /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --no-ipc --branch phoronix-test-suite batch-run stream
+    /home/mev/source/wspy/topdown -o topdown.txt --topdown2 --no-rusage --no-software -no-ipc phoronix-test-suite batch-run stream
+    /home/mev/source/wspy/topdown -o ipc.l2.txt --ipc --cache2 --no-software --no-rusage phoronix-test-suite batch-run stream
+    cat software.branch.txt topdown.txt ipc.l2.txt > topdown.txt
+else
+    /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --branch --no-ipc phoronix-test-suite batch-run stream
+    /home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc --topdown2 --no-rusage --no-software phoronix-test-suite batch-run stream
+    /home/mev/source/wspy/topdown -o l2.float.txt --cache2 --float --no-software --no-ipc --no-rusage phoronix-test-suite batch-run stream
+    cat software.branch.txt ipc.topdown.txt l2.float.txt > topdown.txt
+fi
+    
 
-cat software.branch.txt ipc.topdown.txt l2.float.txt > topdown.txt
+    
+
+
+
+
