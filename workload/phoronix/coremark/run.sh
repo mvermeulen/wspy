462f3b19e61a34b70bdbe90440c935ed86ef0744
Mike Vermeulen <mike@vermeulen.com>
2023-12-30 14:29:58 -0600

    Save PTS output.

--- workload/phoronix/coremark/run.sh
+++ workload/phoronix/coremark/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ $(grep -c Intel /proc/cpuinfo) -gt 0 ]; then
-    /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --no-ipc --branch phoronix-test-suite batch-run coremark
+    /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --no-ipc --branch phoronix-test-suite batch-run coremark 2>&1 | tee coremark.out
     /home/mev/source/wspy/topdown -o topdownx.txt --topdown2 --no-rusage --no-software --no-ipc phoronix-test-suite batch-run coremark
     /home/mev/source/wspy/topdown -o ipc.l2.txt --ipc --cache2 --no-software --no-rusage phoronix-test-suite batch-run coremark
     cat software.branch.txt topdownx.txt ipc.l2.txt > inteltopdown.txt
 else
-    /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --branch --no-ipc phoronix-test-suite batch-run coremark
+    /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --branch --no-ipc phoronix-test-suite batch-run coremark 2>&1 | tee coremark.out
     /home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc --topdown2 --no-rusage --no-software phoronix-test-suite batch-run coremark
     /home/mev/source/wspy/topdown -o l2.float.txt --cache2 --float --no-software --no-ipc --no-rusage phoronix-test-suite batch-run coremark
     cat software.branch.txt ipc.topdown.txt l2.float.txt > amdtopdown.txt
