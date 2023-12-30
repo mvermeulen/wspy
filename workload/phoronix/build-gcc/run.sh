fe66a0e8dbb60b6d1d4f39a3aadeb00d01837833
Mike Vermeulen <mike@vermeulen.com>
2023-12-30 17:37:52 -0600

    Fix typo.

--- workload/phoronix/build-gcc/run.sh
+++ workload/phoronix/build-gcc/run.sh
@@ -8,7 +8,7 @@ else
     /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --branch --no-ipc phoronix-test-suite batch-run build-gcc 2>&1 | tee build-gcc.out
     /home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc --topdown2 --no-rusage --no-software phoronix-test-suite batch-run build-gcc
     /home/mev/source/wspy/topdown -o l2.float.txt --cache2 --float --no-software --no-ipc --no-rusage phoronix-test-suite batch-run build-gcc
-    /home/mev/source/wspy/topdown -o amdtopdown.csv --csv --interval 1 --topdown --no-ipc --no-software --no-rusuage phoronix-test-suite batch-run build-gcc    
+    /home/mev/source/wspy/topdown -o amdtopdown.csv --csv --interval 1 --topdown --no-ipc --no-software --no-rusage phoronix-test-suite batch-run build-gcc    
     cat software.branch.txt ipc.topdown.txt l2.float.txt > amdtopdown.txt
 fi
     
