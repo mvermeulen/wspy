669bae93d5a1d66c2a496b4bc8cb61ef6107b8b9
Mike Vermeulen <mike@vermeulen.com>
2023-12-30 14:04:18 -0600

    Update script to run different things on Intel vs AMD.

--- workload/phoronix/stream/run.sh
+++ workload/phoronix/stream/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $(grep -c Intel /proc/cpuinfo) -gt 0 ]; then
     /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --no-ipc --branch phoronix-test-suite batch-run stream
-    /home/mev/source/wspy/topdown -o topdown.txt --topdown2 --no-rusage --no-software -no-ipc phoronix-test-suite batch-run stream
+    /home/mev/source/wspy/topdown -o topdownx.txt --topdown2 --no-rusage --no-software --no-ipc phoronix-test-suite batch-run stream
     /home/mev/source/wspy/topdown -o ipc.l2.txt --ipc --cache2 --no-software --no-rusage phoronix-test-suite batch-run stream
-    cat software.branch.txt topdown.txt ipc.l2.txt > topdown.txt
+    cat software.branch.txt topdownx.txt ipc.l2.txt > topdown.txt
 else
     /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --branch --no-ipc phoronix-test-suite batch-run stream
     /home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc --topdown2 --no-rusage --no-software phoronix-test-suite batch-run stream
