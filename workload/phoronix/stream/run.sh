603727a3f2df686f0306239c902d0a5fc389824d
Mike Vermeulen <mike@vermeulen.com>
2023-12-30 17:19:26 -0600

    Add --interval runs for --topdown counters.

--- workload/phoronix/stream/run.sh
+++ workload/phoronix/stream/run.sh
@@ -8,6 +8,7 @@ else
     /home/mev/source/wspy/topdown -o software.branch.txt --rusage --software --branch --no-ipc phoronix-test-suite batch-run stream 2>&1 | tee stream.out
     /home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc --topdown2 --no-rusage --no-software phoronix-test-suite batch-run stream
     /home/mev/source/wspy/topdown -o l2.float.txt --cache2 --float --no-software --no-ipc --no-rusage phoronix-test-suite batch-run stream
+    /home/mev/source/wspy/topdown -o amdtopdown.csv --csv --interval 1 --topdown --no-ipc --no-software --no-rusuage phoronix-test-suite batch-run stream
     cat software.branch.txt ipc.topdown.txt l2.float.txt > amdtopdown.txt
 fi
     
