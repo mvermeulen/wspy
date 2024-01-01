5a6ae4ddfb90ee82fb653312dd16e799985cb11f
Mike Vermeulen <mike@vermeulen.com>
2024-01-01 11:02:39 -0600

    Add all entries to the process table.

--- proctree.c
+++ proctree.c
@@ -18,8 +18,133 @@
 
 FILE *process_file = NULL;
 int treeflag = 1;
-int sumflag = 0;
+int statflag = 1;
 int vflag = 0;
+
+/* process_info - maintained for each process */
+struct process_info {
+  unsigned int pid;
+  char *comm; // short version of the command
+  char *cmdline; // long command line
+  struct process_info *parent;
+  struct process_info *children; // linked using the "sibling" relationship
+  struct process_info *prev_sibling; // elder sibling
+  struct process_info *next_sibling; // younger sibling
+};
+
+struct process_info *root_process = NULL;
+
+/* proc_table - table of currently running processes
+ *    Note: Processes are removed once they exit since the pid can be reused.
+ */
+#define NUM_PROC_TABLE_BUCKETS 101
+int num_processes = 0;
+int max_num_processes = 0;
+struct proc_table_entry {
+  unsigned int pid;
+  struct process_info *pinfo;
+  struct proc_table_entry *prev,*next;
+};
+struct proc_table_entry *proc_table[NUM_PROC_TABLE_BUCKETS] = { 0 };
+
+// lookup an entry and insert if necessary
+struct proc_table_entry *lookup_pid(unsigned int pid,int insert){
+  int bucket = pid % NUM_PROC_TABLE_BUCKETS;
+  struct proc_table_entry *pentry;
+
+  for (pentry=proc_table[bucket];pentry;pentry=pentry->next){
+    if (pentry->pid == pid) return pentry;
+  }
+  // not found
+  if (!insert) return NULL;
+
+  pentry = calloc(1,sizeof(struct proc_table_entry));
+  pentry->pid = pid;
+  pentry->pinfo = calloc(1,sizeof(struct process_info));
+  pentry->pinfo->pid = pid;
+  if (proc_table[bucket]){
+    pentry->next = proc_table[bucket];
+    pentry->next->prev = pentry;
+  }
+  proc_table[bucket] = pentry;
+  num_processes++;
+  if (num_processes > max_num_processes)
+    max_num_processes = num_processes;
+  return pentry;
+}
+
+// remove an entry, return 0 if successful
+int remove_pid(unsigned int pid){
+  int bucket = pid % NUM_PROC_TABLE_BUCKETS;
+  int found = 0;
+  
+  struct proc_table_entry *pentry;
+  for (pentry=proc_table[bucket];pentry;pentry=pentry->next){
+    if (pentry->pid == pid){
+      found = 1;
+      break;
+    }
+  }
+  if (!found) return 1;
+
+  if (pentry->next){
+    pentry->next->prev = pentry->prev;
+  }
+  if (pentry->prev){
+    pentry->prev->next = pentry->next;
+  } else {
+    proc_table[bucket] = pentry->next;
+  }
+  num_processes--;
+  free(pentry);
+  return 0;
+}
+
+// format: elapsed pid root
+void handle_root(double elapsed,unsigned int pid){
+  notice("handle_root(%u)\n",pid);
+
+  struct proc_table_entry *pentry = lookup_pid(pid,1);
+  root_process = pentry->pinfo;
+}
+
+// format: elapsed pid fork child_pid
+void handle_fork(double elapsed,unsigned int pid,char *child){
+  debug("handle_fork(%d,%s)\n",elapsed,pid,child);  
+
+  unsigned int child_pid = atoi(child);
+  struct proc_table_entry *pentry = lookup_pid(child_pid,1);
+}
+
+// format: elapsed pid exit </proc/pid/stat>
+void handle_exit(double elapsed,unsigned int pid,char *stat){
+  debug("handle_exit(%d,%s)\n",elapsed,pid,stat);
+
+  int status;
+  struct proc_table_entry *pentry = lookup_pid(pid,0);
+  if (pentry){
+    status = remove_pid(pid);
+    if (status) warning("unable to remove process %d?\n",pid);
+  }
+}
+
+void handle_comm(double elapsed,unsigned int pid,char *comm){
+  debug("handle_comm(%d,%s)\n",elapsed,pid,comm);
+
+  struct proc_table_entry *pentry = lookup_pid(pid,0);
+  if (pentry){
+    pentry->pinfo->comm = strdup(comm);
+  }
+}
+
+void handle_cmdline(double elapsed,unsigned int pid,char *cmdline){
+  debug("handle_cmdline(%d,%s)\n",elapsed,pid,cmdline);
+
+  struct proc_table_entry *pentry = lookup_pid(pid,0);
+  if (pentry){
+    pentry->pinfo->cmdline = strdup(cmdline);
+  }
+}
 
 int main(int argc,char *const argv[],char *const envp[]){
   int opt;
@@ -41,10 +166,10 @@ int main(int argc,char *const argv[],char *const envp[]){
       treeflag = 1;
       break;
     case 's':
-      sumflag = 0;
+      statflag = 0;
       break;
     case 'S':
-      sumflag = 1;
+      statflag = 1;
       break;
     case 'v':
       vflag++;
@@ -87,18 +212,23 @@ int main(int argc,char *const argv[],char *const envp[]){
     if (p2 = strchr(p,'\n')) *p2 = 0; // zap newline
     
     if (!strncmp(p,"fork",4)){
-      notice("handle_fork(%d,%s)\n",elapsed,event_pid,p+5);
+      handle_fork(elapsed,event_pid,p+5);
     } else if (!strncmp(p,"root",4)){
-      notice("handle_root(%d)\n",elapsed,event_pid);      
+      handle_root(elapsed,event_pid);
     } else if (!strncmp(p,"comm",4)){
-      notice("handle_comm(%d,%s)\n",elapsed,event_pid,p+5);
+      handle_comm(elapsed,event_pid,p+5);
     } else if (!strncmp(p,"cmdline",7)){
-      notice("handle_cmdline(%d,%s)\n",elapsed,event_pid,p+8);
+      handle_cmdline(elapsed,event_pid,p+8);      
     } else if (!strncmp(p,"exit",4)){
-      notice("handle_exit(%d,%s)\n",elapsed,event_pid,p+8);
+      handle_exit(elapsed,event_pid,p+5);
     } else {
       warning("unknown command: %4.2f %d %s\n",elapsed,event_pid,p);
     }
+  }
+
+  if (statflag){
+    printf("%d processes running\n",num_processes);
+    printf("%d maximum processes\n",max_num_processes);
   }
   return 0;
 }
