aca0b43d60195af4296a781766ab3ff27123f0a5
Mike Vermeulen <mike@vermeulen.com>
2024-01-03 10:10:00 -0600

    Add timeout while the tree can still hang.

--- workload/phoronix/run_test.sh
+++ workload/phoronix/run_test.sh
@@ -18,7 +18,7 @@ else
     /home/mev/source/wspy/topdown -o ipc.topdown.txt --ipc --topdown2 --no-rusage --no-software phoronix-test-suite batch-run $TESTNAME
     /home/mev/source/wspy/topdown -o l2.float.txt --cache2 --float --no-rusage --no-software phoronix-test-suite batch-run $TESTNAME
     /home/mev/source/wspy/topdown -o amdtopdown.csv --csv --interval 1 --topdown --no-rusage --no-software --no-ipc phoronix-test-suite batch-run $TESTNAME
-    /home/mev/source/wspy/topdown -o treerun.txt --tree process.tree.txt --tree-cmdline --software --no-ipc phoronix-test-suite batch-run $TESTNAME
+    timeout 3600 /home/mev/source/wspy/topdown -o treerun.txt --tree process.tree.txt --tree-cmdline --software --no-ipc phoronix-test-suite batch-run $TESTNAME
     cat software.branch.txt ipc.topdown.txt l2.float.txt > amdtopdown.txt
     /home/mev/source/wspy/workload/phoronix/gnuplot.sh
 fi
