bf29d9661a224d23c8f3cff080d28a8429f74af1
Mike Vermeulen <mike@vermeulen.com>
2024-01-06 19:30:09 -0600

    Tee up for overnight run.

--- workload/phoronix/run_all.sh
+++ workload/phoronix/run_all.sh
@@ -3,20 +3,10 @@ while read bench
 do
     TESTNAME=$bench ./run_test.sh
 done <<-BENCH_LIST
-coremark
-build-gcc
-build-ffmpeg
-build-godot
-build-llvm
 compress-7zip
-gimp
-john-the-ripper
-openscad
-phpbench
-pybench
-pytorch
-rawtherapee
-stream
-tensorflow
-tensorflow-lite
+compress-gzip
+compress-lz4
+compress-rar
+compress-xz
+compress-zstd
 BENCH_LIST
