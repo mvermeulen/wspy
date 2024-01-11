398ad80717a9b2775584aed1b73ed1080c42e9cd
Mike Vermeulen <mike@vermeulen.com>
2024-01-11 12:07:21 -0600

    Update for system metrics.

--- system.c
+++ system.c
@@ -14,9 +14,9 @@ struct system_state {
   double load; // 1 minute load average
   int runnable; // # runnable processes
   struct cpustat {
-    unsigned long usertime, systemtime, idletime, irqtime;
-    unsigned long last_usertime, last_systemtime, last_idletime, last_irqtime;
-    unsigned long prev_usertime, prev_systemtime, prev_idletime, prev_irqtime; } cpu;
+    unsigned long usertime, systemtime, idletime, iowaittime, irqtime;
+    unsigned long last_usertime, last_systemtime, last_idletime, last_iowaittime, last_irqtime;
+    unsigned long prev_usertime, prev_systemtime, prev_idletime, prev_iowaittime, prev_irqtime; } cpu;
 } system_state = { 0 };
 
 // read system-wide state
@@ -46,14 +46,17 @@ void read_system(void){
 	    system_state.cpu.prev_usertime = system_state.cpu.last_usertime;
 	    system_state.cpu.prev_systemtime = system_state.cpu.last_systemtime;
 	    system_state.cpu.prev_idletime = system_state.cpu.last_idletime;
+	    system_state.cpu.prev_iowaittime = system_state.cpu.last_iowaittime;	    
 	    system_state.cpu.prev_irqtime = system_state.cpu.last_irqtime;
 	    system_state.cpu.last_usertime = usertime;
 	    system_state.cpu.last_systemtime = systemtime;
 	    system_state.cpu.last_idletime = idletime;
+	    system_state.cpu.last_idletime = iowait;
 	    system_state.cpu.last_irqtime = irqtime + softirqtime;
 	    system_state.cpu.usertime = system_state.cpu.last_usertime - system_state.cpu.prev_usertime;
 	    system_state.cpu.systemtime = system_state.cpu.last_systemtime - system_state.cpu.prev_systemtime;
 	    system_state.cpu.idletime = system_state.cpu.last_idletime - system_state.cpu.prev_idletime;
+	    system_state.cpu.iowaittime = system_state.cpu.last_iowaittime - system_state.cpu.prev_iowaittime;
 	    system_state.cpu.irqtime = system_state.cpu.last_irqtime - system_state.cpu.prev_irqtime;
 	    break;
 	  }
@@ -76,7 +79,7 @@ void print_system(enum output_format oformat){
   switch(oformat){
   case PRINT_CSV_HEADER:
     if (system_mask & SYSTEM_LOADAVG) fprintf(outfile,"load,runnable,");
-    if (system_mask & SYSTEM_CPU) fprintf(outfile,"cpu,idle,irq,");
+    if (system_mask & SYSTEM_CPU) fprintf(outfile,"cpu,idle,iowait,irq,");
     break;
   case PRINT_CSV:
     if (system_mask & SYSTEM_LOADAVG) fprintf(outfile,"%4.2f,%d,",system_state.load,system_state.runnable);
@@ -85,6 +88,8 @@ void print_system(enum output_format oformat){
 	      (double) (system_state.cpu.usertime+system_state.cpu.systemtime)/elapsed/num_procs);
       fprintf(outfile,"%3.2f%%,",
 	      (double) (system_state.cpu.idletime)/elapsed/num_procs);
+      fprintf(outfile,"%3.2f%%,",
+	      (double) (system_state.cpu.iowaittime)/elapsed/num_procs);      
       fprintf(outfile,"%3.2f%%,",
 	      (double) (system_state.cpu.irqtime)/elapsed/num_procs);      
     }
@@ -98,7 +103,9 @@ void print_system(enum output_format oformat){
       fprintf(outfile,"cpu                  %3.2f%%\n",
 	      (double) (system_state.cpu.usertime+system_state.cpu.systemtime)/elapsed/num_procs);
       fprintf(outfile,"idle                 %3.2f%%\n",
-	      (double) (system_state.cpu.usertime+system_state.cpu.systemtime)/elapsed/num_procs);
+	      (double) (system_state.cpu.idletime/elapsed/num_procs));
+      fprintf(outfile,"io                   %3.2f%%\n",
+	      (double) (system_state.cpu.iowaittime/elapsed/num_procs));      
       fprintf(outfile,"irq                  %3.2f%%\n",
 	      (double) (system_state.cpu.usertime+system_state.cpu.systemtime)/elapsed/num_procs);      
     }
