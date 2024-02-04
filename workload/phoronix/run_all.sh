571cd80b2704058311a97327a0eaa1a487d8e05c
Mike Vermeulen <mike@vermeulen.com>
2024-02-04 17:50:59 -0600

    Update benchmarks to rerun for newer format.

--- workload/phoronix/run_all.sh
+++ workload/phoronix/run_all.sh
@@ -3,17 +3,7 @@ while read bench
 do
     TESTNAME=$bench ./run_test.sh
 done <<-BENCH_LIST
-build-clash
-build-eigen
-build-erlang
-build-gdb
-build-gem5
-build-imagemagick
-build-mesa
-build-python
-build-wasmer
-build2
-bullet
-byte
-plaidml
+apache-iotdb-perf
+appleseed
+avifenc
 BENCH_LIST
