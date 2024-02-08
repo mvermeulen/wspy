ec231eb472a78a9abbb173cd43cb4d4e2a665674
Mike Vermeulen <mike@vermeulen.com>
2024-02-08 14:13:53 -0600

    Add benchmarks for 2/8 article.

--- workload/phoronix/run_all.sh
+++ workload/phoronix/run_all.sh
@@ -3,12 +3,9 @@ while read bench
 do
     TESTNAME=$bench ./run_test.sh
 done <<-BENCH_LIST
-easywave
-embree-perf
-gimp
-gromacs
-java-scimark2
-llama-cpp-perf
-openjpeg
-phpbench
+graph500
+helsing
+hpcg-perf
+mt-dgemm
+primesieve
 BENCH_LIST
