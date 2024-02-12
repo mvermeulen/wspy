cf2226bc7d0487aa57ac454e901538eee4563cec
Mike Vermeulen <mike@vermeulen.com>
2024-02-12 04:04:31 -0600

    Update next run list.

--- workload/phoronix/run_all.sh
+++ workload/phoronix/run_all.sh
@@ -3,19 +3,6 @@ while read bench
 do
     TESTNAME=$bench ./run_test.sh
 done <<-BENCH_LIST
-compress-gzip
-darktable
-dav1d
-dolfyn
-encode-wavpack
-glibc-bench
-gmpbench
-gnupg
-himeno
-hmmer
-incompact3d
-libreoffice
-lulesh
-mnn
-tensorflow-perf
+hackbench
+libxsmm
 BENCH_LIST
