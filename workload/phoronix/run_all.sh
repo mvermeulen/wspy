424bc2766994de886512e0150d752496cb4905a4
Mike Vermeulen <mike@vermeulen.com>
2024-02-12 06:56:35 -0600

    Queue up quick running workloads.

--- workload/phoronix/run_all.sh
+++ workload/phoronix/run_all.sh
@@ -3,6 +3,26 @@ while read bench
 do
     TESTNAME=$bench ./run_test.sh
 done <<-BENCH_LIST
-hackbench
-libxsmm
+mpcbench
+m-queens
+mrbayes
+namd
+node-web-tooling
+n-queens
+octave-benchmark
+openscad
+perl-benchmark
+povray
+pybench
+quantlib
+rawtherapee
+rbenchmark
+scimark2
+selenium-perf
+sqlite-perf
+stream
+svt-av1
+svt-vp9
+v-ray
+x264
 BENCH_LIST
