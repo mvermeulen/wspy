562ec5371a2cfe1c8cca95fa00ff67658a878d52
Mike Vermeulen <mike@vermeulen.com>
2024-06-10 11:15:03 -0500

    Add cpu2017.

---
+++ workload/cpu2017/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TESTNAME=500.perlbench_r ./run_test.sh
+TESTNAME=502.gcc_r ./run_test.sh
+TESTNAME=505.mcf_r ./run_test.sh
+TESTNAME=520.omnetpp_r ./run_test.sh
+TESTNAME=523.xalancbmk_r ./run_test.sh
+TESTNAME=523.x265_r ./run_test.sh
+TESTNAME=531.deepsjeng_r ./run_test.sh
+TESTNAME=541.leela_r ./run_test.sh
+TESTNAME=548.exchange2_r ./run_test.sh
+TESTNAME=557.xz_r ./run_test.sh
