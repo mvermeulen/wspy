f5b65581de5ac6cb6ba4fc0fec88ecacd9ccc696
Mike Vermeulen <mike@vermeulen.com>
2024-10-10 15:45:06 -0600

    Create phoronix cluster example.

--- workload/phoronix/run_all.sh
+++ workload/phoronix/run_all.sh
@@ -3,6 +3,33 @@ while read bench
 do
     TESTNAME=$bench ./run_test.sh
 done <<-BENCH_LIST
-whisperfile
-xnnpack
+ospray
+compress-xz
+quicksilver
+x265
+coremark
+tscp
+build-gcc
+phpbench
+lammps
+compress-zstd
+simdjson
+perl-benchmark
+ffmpeg
+compress-gzip
+povray
+pytorch
+tensorflow
+darktable
+compress-7zip
+lulesh
+minibude
+ebizzy
+openssl
+build-php
+pybench
+apache
+indigobench
+lczero
+gromacs
 BENCH_LIST
