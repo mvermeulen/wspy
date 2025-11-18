0158f812b450305fe0978ffc8e8d869c40ed7fb5
Mike Vermeulen <mike@vermeulen.com>
2025-11-17 20:04:47 -0600

    add initialization and finalization for amd_smi

--- amd_smi.c
+++ amd_smi.c
@@ -1,20 +1,31 @@
 #if AMDGPU
 
 #include "amd_smi/amdsmi.h"
+#include "error.h"
 
 void amd_smi_initialize(void)
 {
-	/* Placeholder for AMD SMI initialization helpers */
+	amdsmi_status_t ret;
+	
+	ret = amdsmi_init(AMDSMI_INIT_AMD_GPUS);
+	if (ret != AMDSMI_STATUS_SUCCESS) {
+		error("amdsmi_init failed with status %d", ret);
+	}
 }
 
 void amd_smi_finalize(void)
 {
-	/* Placeholder for AMD SMI finalization helpers */
+	amdsmi_status_t ret;
+	
+	ret = amdsmi_shut_down();
+	if (ret != AMDSMI_STATUS_SUCCESS) {
+		error("amdsmi_shut_down failed with status %d", ret);
+	}
 }
 
 #if TEST_AMD_SMI
 int main(void){
-    
+
 	amd_smi_initialize();
 	amd_smi_finalize();
 	return 0;
