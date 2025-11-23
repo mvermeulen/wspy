b64af27d5a15b70f31fa24251d2d3282edc1f073
Mike Vermeulen <mike@vermeulen.com>
2025-11-22 19:03:15 -0600

    add functions to print gpu sysfs metrics

--- amd_sysfs.c
+++ amd_sysfs.c
@@ -2,6 +2,10 @@
 #include "amd_sysfs.h"
 #include "error.h"
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
 /* Flag indicating presence of gpu_busy_percent sysfs entry */
 static int amd_sysfs_has_gpu_busy = 0;
@@ -19,7 +23,7 @@ static int amd_sysfs_has_gpu_metrics = 0;
 	if (access(path2, R_OK) == 0) {
 		amd_sysfs_has_gpu_metrics = 1;
 	} else {
-		error("AMD SYSFS gpu_metrics not found");
+		error("AMD SYSFS gpu_metrics not found\n");
 	}
 }
 
@@ -27,10 +31,107 @@ void amd_sysfs_finalize(void)
 {
 }
 
+int amd_sysfs_gpu_busy_percent(void)
+{
+	FILE *fp;
+	int value = 0;
+	
+	fp = fopen("/sys/class/drm/card1/device/gpu_busy_percent", "r");
+	if (fp) {
+		if (fscanf(fp, "%d", &value) == 1) {
+			debug("GPU busy percent: %d\n", value);
+		}
+		fclose(fp);
+	}
+	return value;
+}
+
+void amd_sysfs_gpu_metrics(void)
+{
+	FILE *fp;
+	struct amd_sysfs_metrics_header header;
+	struct amd_sysfs_gpu_metrics_v1_0 metrics_v1;
+	struct amd_sysfs_gpu_metrics_v2_0 metrics_v2;
+	struct amd_sysfs_gpu_metrics_v3_0 metrics_v3;
+	size_t bytes_read;
+	
+	fp = fopen("/sys/class/drm/card1/device/gpu_metrics", "rb");
+	if (!fp) {
+		return;
+	}
+	
+	bytes_read = fread(&header, 1, sizeof(header), fp);
+	if (bytes_read < sizeof(header)) {
+		fclose(fp);
+		return;
+	}
+	
+	debug("GPU metrics header:\n");
+	debug("  Structure size: %u\n", header.structure_size);
+	debug("  Format revision: %u\n", header.format_revision);
+	debug("  Content revision: %u\n", header.content_revision);
+	
+	rewind(fp);
+	
+	if (header.format_revision == 1) {
+		memset(&metrics_v1, 0, sizeof(metrics_v1));
+		bytes_read = fread(&metrics_v1, 1, sizeof(metrics_v1), fp);
+		debug("GPU metrics v1.0: read %zu bytes\n", bytes_read);
+		debug("  Temperature edge: %u C\n", metrics_v1.temperature_edge);
+		debug("  Temperature hotspot: %u C\n", metrics_v1.temperature_hotspot);
+		debug("  Average GFX activity: %u%%\n", metrics_v1.average_gfx_activity);
+		debug("  Average socket power: %u W\n", metrics_v1.average_socket_power);
+	} else if (header.format_revision == 2) {
+		struct amd_sysfs_gpu_metrics_v2_0 metrics_v2;
+		memset(&metrics_v2, 0, sizeof(metrics_v2));
+		bytes_read = fread(&metrics_v2, 1, sizeof(metrics_v2), fp);
+		debug("GPU metrics v2.0: read %zu bytes\n", bytes_read);
+		debug("  Temperature GFX: %.1f C\n", metrics_v2.temperature_gfx / 100.0);
+		debug("  Temperature SOC: %.1f C\n", metrics_v2.temperature_soc / 100.0);
+		debug("  Average GFX activity: %u%%\n", metrics_v2.average_gfx_activity);
+		debug("  Average MM activity: %u%%\n", metrics_v2.average_mm_activity);
+		debug("  Average socket power: %.2f W\n", metrics_v2.average_socket_power / 1000.0);
+		debug("  Average CPU power: %.2f W\n", metrics_v2.average_cpu_power / 1000.0);
+		debug("  Average SOC power: %.2f W\n", metrics_v2.average_soc_power / 1000.0);
+		debug("  Average GFX power: %.2f W\n", metrics_v2.average_gfx_power / 1000.0);
+		debug("  Average GFXCLK frequency: %u MHz\n", metrics_v2.average_gfxclk_frequency);
+		debug("  Average SOCCLK frequency: %u MHz\n", metrics_v2.average_socclk_frequency);
+		debug("  Average FCLK frequency: %u MHz\n", metrics_v2.average_fclk_frequency);
+		debug("  Average UCLK frequency: %u MHz\n", metrics_v2.average_uclk_frequency);
+		debug("  Current GFXCLK: %u MHz\n", metrics_v2.current_gfxclk);
+		debug("  Throttle status: 0x%08x\n", metrics_v2.throttle_status);
+		debug("  Fan PWM: %u\n", metrics_v2.fan_pwm);
+	} else if (header.format_revision == 3) {
+		memset(&metrics_v3, 0, sizeof(metrics_v3));
+		bytes_read = fread(&metrics_v3, 1, sizeof(metrics_v3), fp);
+		debug("GPU metrics v3.0: read %zu bytes\n", bytes_read);
+		debug("  Temperature GFX: %.1f C\n", metrics_v3.temperature_gfx / 100.0);
+		debug("  Temperature SOC: %.1f C\n", metrics_v3.temperature_soc / 100.0);
+		debug("  Temperature skin: %.1f C\n", metrics_v3.temperature_skin / 100.0);
+		debug("  Average GFX activity: %u%%\n", metrics_v3.average_gfx_activity);
+		debug("  Average VCN activity: %u%%\n", metrics_v3.average_vcn_activity);
+		debug("  Average socket power: %.2f W\n", metrics_v3.average_socket_power / 1000.0);
+		debug("  Average APU power: %.2f W\n", metrics_v3.average_apu_power / 1000.0);
+		debug("  Average GFX power: %.2f W\n", metrics_v3.average_gfx_power / 1000.0);
+		debug("  Average GFXCLK frequency: %u MHz\n", metrics_v3.average_gfxclk_frequency);
+		debug("  Average SOCCLK frequency: %u MHz\n", metrics_v3.average_socclk_frequency);
+		debug("  Average FCLK frequency: %u MHz\n", metrics_v3.average_fclk_frequency);
+		debug("  Average UCLK frequency: %u MHz\n", metrics_v3.average_uclk_frequency);
+		debug("  Current GFX max freq: %u MHz\n", metrics_v3.current_gfx_maxfreq);
+	} else {
+		debug("Unknown GPU metrics format revision: %u\n", header.format_revision);
+	}
+	
+	fclose(fp);
+}
+
 #ifdef TEST_AMD_SYSFS
 int main(void)
 {
+	set_error_level(ERROR_LEVEL_DEBUG);
 	amd_sysfs_initialize();
+	amd_sysfs_gpu_busy_percent();
+	amd_sysfs_gpu_metrics();
 	amd_sysfs_finalize();
 	return 0;
 }
--- amd_sysfs.h
+++ amd_sysfs.h
@@ -1,5 +1,129 @@
  #if AMDGPU
+#include <stdint.h>
+
+// Helper to match the kernel struct layout
+// See: drivers/gpu/drm/amd/include/kgd_pp_interface.h in Linux source
+struct amd_sysfs_metrics_header {
+    uint16_t structure_size;
+    uint8_t format_revision;
+    uint8_t content_revision;
+};
+
+// Simplified v1.0 struct
+struct amd_sysfs_gpu_metrics_v1_0 {
+    struct amd_sysfs_metrics_header common_header;
+    uint64_t system_clock_counter;
+    uint16_t temperature_edge;
+    uint16_t temperature_hotspot;
+    uint16_t temperature_mem;
+    uint16_t temperature_vrgfx;
+    uint16_t temperature_vrsoc;
+    uint16_t temperature_vrmem;
+    uint16_t average_gfx_activity;
+    uint16_t average_umc_activity;
+    uint16_t average_mm_activity;
+    uint16_t average_socket_power;
+    uint64_t energy_accumulator;
+};
+
+struct amd_sysfs_gpu_metrics_v2_0 {
+	struct amd_sysfs_metrics_header	common_header;
+
+	uint16_t			temperature_gfx;
+	uint16_t			temperature_soc;
+	uint16_t			temperature_core[8];
+	uint16_t			temperature_l3[2];
+
+	uint16_t			average_gfx_activity;
+	uint16_t			average_mm_activity;
+
+	uint64_t			system_clock_counter;
+
+	uint16_t			average_socket_power;
+	uint16_t			average_cpu_power;
+	uint16_t			average_soc_power;
+	uint16_t			average_gfx_power;
+	uint16_t			average_core_power[8];
+
+	uint16_t			average_gfxclk_frequency;
+	uint16_t			average_socclk_frequency;
+	uint16_t			average_uclk_frequency;
+	uint16_t			average_fclk_frequency;
+	uint16_t			average_vclk_frequency;
+	uint16_t			average_dclk_frequency;
+
+	uint16_t			current_gfxclk;
+	uint16_t			current_socclk;
+	uint16_t			current_uclk;
+	uint16_t			current_fclk;
+	uint16_t			current_vclk;
+	uint16_t			current_dclk;
+	uint16_t			current_coreclk[8];
+	uint16_t			current_l3clk[2];
+
+	uint32_t			throttle_status;
+
+	uint16_t			fan_pwm;
+
+	uint16_t			padding[3];
+};
+
+struct amd_sysfs_gpu_metrics_v3_0 {
+	struct amd_sysfs_metrics_header	common_header;
+	uint16_t			temperature_gfx;
+	uint16_t			temperature_soc;
+	uint16_t			temperature_core[16];
+	uint16_t			temperature_skin;
+
+	uint16_t			average_gfx_activity;
+	uint16_t			average_vcn_activity;
+	uint16_t			average_ipu_activity[8];
+	uint16_t			average_core_c0_activity[16];
+	uint16_t			average_dram_reads;
+	uint16_t			average_dram_writes;
+	uint16_t			average_ipu_reads;
+	uint16_t			average_ipu_writes;
+
+	uint64_t			system_clock_counter;
+
+	uint32_t			average_socket_power;
+	uint16_t			average_ipu_power;
+	uint32_t			average_apu_power;
+	uint32_t			average_gfx_power;
+	uint32_t			average_dgpu_power;
+	uint32_t			average_all_core_power;
+	uint16_t			average_core_power[16];
+	uint16_t			average_sys_power;
+	uint16_t			stapm_power_limit;
+	uint16_t			current_stapm_power_limit;
+
+	uint16_t			average_gfxclk_frequency;
+	uint16_t			average_socclk_frequency;
+	uint16_t			average_vpeclk_frequency;
+	uint16_t			average_ipuclk_frequency;
+	uint16_t			average_fclk_frequency;
+	uint16_t			average_vclk_frequency;
+	uint16_t			average_uclk_frequency;
+	uint16_t			average_mpipu_frequency;
+
+	uint16_t			current_coreclk[16];
+	uint16_t			current_core_maxfreq;
+	uint16_t			current_gfx_maxfreq;
+
+	uint32_t			throttle_residency_prochot;
+	uint32_t			throttle_residency_spl;
+	uint32_t			throttle_residency_fppt;
+	uint32_t			throttle_residency_sppt;
+	uint32_t			throttle_residency_thm_core;
+	uint32_t			throttle_residency_thm_gfx;
+	uint32_t			throttle_residency_thm_soc;
+
+	uint32_t			time_filter_alphavalue;
+};
+
+
  void amd_sysfs_initialize(void);
  void amd_sysfs_finalize(void);
+ int amd_sysfs_gpu_busy_percent(void);
+ void amd_sysfs_gpu_metrics(void);
  #endif
-// /sys/class/drm/card0/device/gpu_busy_percent
